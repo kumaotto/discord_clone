973fc51a5da5b68e7ee2ada2e7c165a31c95ea14
kumaotto <kwkomaki.kom@gmail.com>
2023-04-27 23:57:39 +0900

    Add Channel List

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
 
 const Sidebar = () => {
   return (
@@ -13,12 +15,31 @@ const Sidebar = () => {
             <img src="./logo512.png" className='' alt="" />
           </div>
         </div>
+
         <div className="bg-side-menu w-72">
           <div className='text-white flex items-center px-5 py-5 justify-between'>
             <h3>Discord</h3>
             <ExpandMoreIcon />
           </div>
+          
+          <div className='px-3'>
+            <div className='flex justify-between mt-1 text-white items-center'>
+              <div className='flex'>
+                <ExpandMoreIcon />
+                <h4>プログラミングチャンネル</h4>
+              </div>
+              <AddIcon className='cursor-pointer' />
+            </div>
+
+            <div>
+              <SidebarChannel />
+              <SidebarChannel />
+              <SidebarChannel />
+              <SidebarChannel />
+            </div>
+          </div>
         </div>
+
       </div>
     </div>
   )
---
+++ src/components/sidebar/SidebarChannel.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SidebarChannel = () => {
+  return (
+    <div className='pl-4 m-1'>
+      <h4 className='text-gray-500 flex items-center p-1 cursor-pointer hover:text-white'>
+        <span className='mr-1'>#</span>
+        AWS
+      </h4>
+    </div>
+  )
+}
+
+export default SidebarChannel
