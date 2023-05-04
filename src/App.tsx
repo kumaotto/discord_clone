da3fd348392359559cc78db6b831dd628a92873c
kumaotto <kwkomaki.kom@gmail.com>
2023-05-04 22:11:46 +0900

    Add hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
 import Login from './components/Login/Login';
 import { useSelector } from 'react-redux';
+import { useAppSelector } from './app/hooks';
 
 function App() {
 
-  // const user = useSelector((state) => state.user.user)
-  const user = null;
+  const user = useAppSelector((state) => state.user)
 
   return (
     <div className="flex">
