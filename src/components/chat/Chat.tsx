882f9a25fc45adf609ef188bf72253b1289b5cd8
kumaotto <kwkomaki.kom@gmail.com>
2023-05-05 21:45:44 +0900

    Add sending message function

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatHeader from './ChatHeader'
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
@@ -7,11 +7,61 @@ import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 import ChatMessage from './ChatMessage';
 import { useAppSelector } from '../../app/hooks';
+import { CollectionReference, DocumentData, Timestamp, addDoc, collection, onSnapshot, serverTimestamp } from 'firebase/firestore';
+import { db } from '../../firebase';
+
+interface Messages {
+  timestamp: Timestamp;
+  message: string;
+  user: {
+    uid: string;
+    image: string;
+    email: string;
+    displayName: string;
+  }
+}
 
 const Chat = () => {
 
+  const [inputText, setInputText] = useState<string>("");
+  const [messages, setMessages] = useState<Messages[]>([]);
   const channelName = useAppSelector(state => state.channel.channelName);
-  console.log(channelName)
+  const channelId = useAppSelector(state => state.channel.channelId)
+  const user = useAppSelector(state => state.user.user)
+
+  useEffect(() => {
+    let collectionRef = collection(db, "channels", String(channelId), "messages");
+    
+    onSnapshot(collectionRef, (snapshot) => {
+      let result: Messages[] = [];
+      snapshot.docs.forEach((doc) => {
+        result.push({
+          timestamp: doc.data().timestamp,
+          message: doc.data().message,
+          user: doc.data().user,
+        })
+      })
+      setMessages(result)
+      console.log(result)
+    })
+  }, [channelId]);
+
+  const sendMessage = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    const collectionRef: CollectionReference<DocumentData> = collection(
+      db,
+      "channels",
+      String(channelId),
+      "messages"
+    );
+
+    await addDoc(collectionRef, {
+      message: inputText,
+      timestamp: serverTimestamp(),
+      user: user,
+    })
+  }
 
   return (
     <div className='flex flex-col flex-grow bg-chat'>
@@ -27,10 +77,20 @@ const Chat = () => {
 
       <div className='flex item-center justify-between p-4 bg-chat-input rounded-md m-5 text-gray-200'>
         <AddCircleOutlineIcon />
-        {/* textareaに変更する */}
         <form className='flex-grow' action="">
-          <input className='px-2 bg-transparent text-white break-words w-full' type="text" placeholder='#Textへメッセージを送信'/>
-          <button className='hidden' type='submit'>送信</button>
+          <input
+            className='px-2 bg-transparent text-white break-words w-full outline-none'
+            type="text"
+            placeholder='#Textへメッセージを送信'
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value)}
+          />
+          <button
+            className='hidden'
+            type='submit'
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => sendMessage(e)}
+          >
+            送信
+          </button>
         </form>
 
         <div>
